81d2c66f7f06fc6c26b70de9bfdd32affc524d8c
andros25 <zsgibbs@outlook.com>
2021-08-04 02:08:06 -0400

    Updated version of acre

--- acre.java
+++ acre.java
@@ -1,21 +1,33 @@
 public class acre extends andria {
     private String words;
+    private int strength;
 
     public acre() {
         words = "I have the high ground!";
+        strength = 7;
     }
 
-    public void getWords() {
-        pTextln(words);
+    public void setStrength() {
+        strength = rInt("What is your strength level?");
+    }
+
+    public Integer getStrength() {
+        return strength;
     }
 
     public void setWords() {
         words = "You under-estimate my powers!!!";
     }
 
+    public void getWords() {
+        pTextln(words);
+    }
+
     public void dialogue() {
         getWords();
         setWords();
         getWords();
+        setStrength();
+        getStrength();
     }
 }
