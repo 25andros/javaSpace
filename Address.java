9e8996e82860d334ebc21ac9ec57a848201d677f
andros25 <zsgibbs@outlook.com>
2021-08-04 11:31:54 -0400

    Starter shell

---
+++ Address.java
@@ -0,0 +1,17 @@
+public class Address extends andria {
+
+    private Integer numSt;
+    private String st;
+    private String city;
+    private String state;
+    private Integer zip;
+
+    public Address() {
+
+    }
+
+    public void drivercode() {
+
+    }
+
+}
